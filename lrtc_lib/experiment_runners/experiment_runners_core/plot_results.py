0e1596042d13213666d5507bc08ebde37ef79bc0
César González <ceguel@gmail.com>
2021-06-15 13:37:54 +0200

    Improve plot results

--- lrtc_lib/experiment_runners/experiment_runners_core/plot_results.py
+++ lrtc_lib/experiment_runners/experiment_runners_core/plot_results.py
@@ -10,6 +10,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from lrtc_lib.active_learning.strategies import strategy_names_in_paper
+from lrtc_lib.experiment_runners.experiment_runner import ExperimentRunner
+
 sns.set_theme()
 sns.set_palette("tab10")
 
@@ -23,7 +26,7 @@ def plot_metric(dataset, metric, df, output_path):
     x_col = "train total count"
     for model in models:
         for al in als:
-            if al == "no_active_learning":
+            if al == ExperimentRunner.NO_AL:
                 continue
             model_df_all = df[(df["model"] == model) & ((df["AL"] == al) | (df["AL"] == "no_active_learning"))]
             model_df = model_df_all[[x_col, metric]]
@@ -31,14 +34,18 @@ def plot_metric(dataset, metric, df, output_path):
             model_df = model_df.groupby(x_col, as_index=False).mean()
             x = model_df[x_col]
             y = model_df[metric]
-            ax = sns.lineplot(x=x, y=y, label=f'{al}_{model}')
+            al_name = strategy_names_in_paper[al]
+            ax = sns.lineplot(x=x, y=y, label=f'{al_name}_{model}')
     plt.title(dataset)
     plt.savefig(os.path.join(output_path, f"{dataset}_{metric}"))
     plt.show()
     plt.close()
 
 
-def plot_results(path, metrics=["accuracy", "f1"]):
+def plot_results(path, metrics=None):
+    all_metrics = ["accuracy", "f1"]
+    if metrics is None:
+        metrics = all_metrics
     df = pd.read_csv(path)
     datasets = df["dataset"].unique()
     for dataset in datasets:
@@ -48,6 +55,9 @@ def plot_results(path, metrics=["accuracy", "f1"]):
 
 
 if __name__ == '__main__':
-    from lrtc_lib.definitions import ROOT_DIR
-    results_file_path = "output/experiments/balanced_NB_20201020_1730/results/balanced_NB_all_repeats.csv"
-    plot_results(os.path.join(ROOT_DIR, results_file_path))
+    import sys
+    if len(sys.argv) != 2:
+        print("Invalid number of arguments.")
+        print(f"Usage: {sys.argv[0]} <file.csv>")
+        exit(1)
+    plot_results(sys.argv[1])
